dc9ed770d5298d64125a70e0c02dc4b6777609b6
Louis Borsu <sat@satprod.net>
2015-01-04 16:52:40 +0100

    HTTPS-force Added

--- app.js
+++ app.js
@@ -57,5 +57,17 @@ app.use(function(err, req, res, next) {
     });
 });
 
+app.use(function(req, res, next) {
+  var schema = req.headers['x-forwarded-proto'];
+
+  if (schema === 'https') {
+    // Already https; don't do anything special.
+    next();
+  } else {
+    // Redirect to https.
+    res.redirect('https://' + req.headers.host + req.url);
+  }
+});
+
 
 module.exports = app;
