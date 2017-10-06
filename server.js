9619f49fb12e917e4ccc14c1fb739290f856d9c7
Louis Borsu <sat@satprod.net>
2017-10-06 09:23:38 +0200

    feat(heroku): modify for heroku

--- server.js
+++ server.js
@@ -2,6 +2,6 @@
 var app = require('./app');
 
 // Start the server.
-var port = process.env.CHACHOU_SERVICE_PORT || 8080;
+var port = process.env.PORT || process.env.httpPort || 3000;
 var ip = process.env.CHACHOU_SERVICE_HOST || '127.0.0.1';
 app.listen(port, ip);
