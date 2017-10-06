378c3dd226ba6e8eda989afde78c2ea73f904774
Louis Borsu <sat@satprod.net>
2017-10-06 09:27:49 +0200

    fix(heroku): is my friend

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var app = require('./app');
 
 // Start the server.
-var port = process.env.PORT || process.env.httpPort || 3000;
-var ip = process.env.CHACHOU_SERVICE_HOST || '127.0.0.1';
-app.listen(port, ip);
+var port = process.env.PORT || 5000;
+app.listen(port, function () {
+  console.log('Node app is running on port', app.get('port'));
+});
