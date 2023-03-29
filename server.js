9060f673558d9bbd38c52dea38329515a2e562e1
Charlotte Tordeur <charlottetordeur@gmail.com>
2023-03-29 18:09:52 +0200

    feat(2023): no more server

--- server.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-var app = require('./app');
-
-// Start the server.
-var port = process.env.PORT || 5000;
-app.listen(port, function () {
-  console.log('Node app is running on port', app.get('port'));
-});
--- src/public/js/lang.js
+++
@@ -1,1 +0,0 @@
-"use strict";$(document).ready(function(){$(".click-fr").click(function(){window.location="https://www.charlottetordeur.be/fr"}),$(".click-en").click(function(){window.location="https://www.charlottetordeur.be/en"})});
